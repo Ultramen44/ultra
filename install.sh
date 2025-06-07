ed914f3e20f6eb6865db933d8c121768a60bbc7d
Ultramen44 <ramampu694@gmail.com>
2025-06-07 14:25:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -330,7 +330,7 @@ TIMEZONE=$(printf '%(%H:%M:%S)T')
 <code>Time     :</code><code>$TIMEZONE</code>
 <code>Exp Sc.  :</code><code>$exp</code>
 <code>─────────────────────────────</code>
-<b> ILHAM GANTENG VPN SCRIPT  </b>
+<b> WONG NDEZO AUTO SCRIPT  </b>
 <code>─────────────────────────────</code>
 <i>Automatic Notifications From Github</i>
 "'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ","url":"t.me/Ultramenstore"}]]}' 
